5ecc49cb59810bfdf5d86d4b7da89d6cf5b559b7
Tushar Handa <tusharhanda31@gmail.com>
2026-03-19 19:02:27 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
-// src/firebase.js
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
+firebase.initializeApp({
+  apiKey: "AIzaSyC6-RtO6zU4MXl-EbqzEo3DJz7-_jU0zVQ",
+  authDomain: "omnishop-66d18.firebaseapp.com",
+  projectId: "omnishop-66d18",
+  messagingSenderId: "33212048795",
+  appId: "1:33212048795:web:ae42691c53e188fadded3e"
+});
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  storageBucket: "omnishop-66d18.firebasestorage.app"
- 
-};
+const messaging = firebase.messaging();
 
-const app = initializeApp(firebaseConfig);
-
-export const messaging = getMessaging(app);
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body
+  });
+});
